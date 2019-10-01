cb4eaa2d131521f1b46ca16545d03bb6d947c8e1
Peter Vandenberk <peter.vandenberk@simplybusiness.co.uk>
2019-10-01 17:28:38 +0100

    Update lib/flip_fab/version.rb
    Co-Authored-By: Lukas Oberhuber <lukas.oberhuber@simplybusiness.co.uk>

--- lib/flip_fab/version.rb
+++ lib/flip_fab/version.rb
@@ -1,5 +1,5 @@
 module FlipFab
-  GEM_VERSION = '1.0.3'.freeze
+  GEM_VERSION = '1.0.4'.freeze
 
   # SB-specific versioning "algorithm" to accomodate Jenkins / gemstash
   VERSION = if ENV['GEM_PRE_RELEASE'].nil? || ENV['GEM_PRE_RELEASE'].empty?
