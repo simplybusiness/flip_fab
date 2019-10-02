d891649b33db8ae7c01cf5037cebe656975247bb
Lukas Oberhuber <lukas.oberhuber@simplybusiness.co.uk>
2019-10-02 12:07:30 +0100

    Localize version hack

--- lib/flip_fab/version.rb
+++ lib/flip_fab/version.rb
@@ -1,7 +1,3 @@
 module FlipFab
-  VERSION = '1.0.5' # rubocop:disable Style/MutableConstant
-
-  # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
-  VERSION << '.' << ENV['GEM_PRE_RELEASE'].strip \
-  unless ENV.fetch('GEM_PRE_RELEASE', '').strip.empty?
+  VERSION = '1.0.6'
 end
