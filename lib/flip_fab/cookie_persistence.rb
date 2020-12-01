3199883cb121f4d5682bfdc495db1e6bad7ed9a7
Kannan Ganesan <kannan.ganesan@simplybusiness.co.uk>
2020-12-01 22:58:04 +0000

    removing unused cookie_domain

--- lib/flip_fab/cookie_persistence.rb
+++ lib/flip_fab/cookie_persistence.rb
@@ -14,7 +14,6 @@ module FlipFab
     end
 
     def write(state)
-      cookie_domain = ".#{top_level_domain}" unless top_level_domain.nil?
       context.response.set_cookie key, value:   state,
                                        expires: cookie_expiration,
                                        path:    COOKIE_PATH
