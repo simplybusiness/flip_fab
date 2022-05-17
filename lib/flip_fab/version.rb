83042b754d68774b08e84ce2110b0fc9b714fcfc
Bud (Mugurel) Chirica <mugurel.chirica@simplybusiness.co.uk>
2022-05-17 16:43:34 +0100

    update codeowners

--- lib/flip_fab/version.rb
+++ lib/flip_fab/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module FlipFab
-  base = '1.0.12'
+  base = '1.0.13'
 
   # SB-specific versioning "algorithm" to accommodate BNW/Jenkins/gemstash
   VERSION = (pre = ENV.fetch('GEM_PRE_RELEASE', '')).empty? ? base : "#{base}.#{pre}"
